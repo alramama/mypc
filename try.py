4d6923be6a9d72a467a02143845f0d3e27a60c55
alramama <74817864+alramama@users.noreply.github.com>
2022-12-29 15:43:27 +0300

    Update try.py

--- try.py
+++ try.py
@@ -1,19 +1,38 @@
+import ast
+import time
 import pandas as pd
-extracted_file=[]
-a=['Camera','ISS','Door','Card','Data','blocker-','Video','CCTV','Held','Access','Civil','Crash','Gate','Generic']
-for keys in range(len(a)):
-    file = pd.read_excel('Jubail Desalination Plant.xlsx')
-    df_sheet_index = pd.read_excel('Jubail Desalination Plant.xlsx', sheet_name=1)
-    #print(df_sheet_index)
-    data = pd.DataFrame(df_sheet_index)
-    rows = data.to_numpy()
-    for i in rows:
-        search = (str(i[0]).split())
-        for word in search:
-            if a[keys] == word:
-                datas= (str(a[keys]).count(a[keys]),a[keys],i)
-                extracted_file.append(data)
-data = pd.DataFrame(extracted_file)
-data.to_excel("output.xlsx")
+import pdfplumber
+import arabic_reshaper
+from bidi.algorithm import get_display
+from tkinter import filedialog
+def trin3():
+    m = []
+    #open_file = filedialog.askopenfilename()
+    pdf = pdfplumber.open('BOQ_Bid.pdf')
+    n = len(pdf.pages)
+    for page in range(n):
+        f = pdf.pages[page].extract_tables()
+        reshaped_text = arabic_reshaper.reshape(str(f).replace("\n",""))
+        f1 = get_display(reshaped_text)
+        f2 = ast.literal_eval(f1)
+        for i in range(len(list(f2))):
+            for j in range(len(f2[i])):
+                Key_word = '(CIDX)'
+                a = (list(filter(None, f2[i][j])))
+                if len(a) == 3:
+                    #print(a)
+                    bid_word = ((str(a[0]).split()))
+                    #print(bid_word)
+                    for word in bid_word:
+                        if word == Key_word:
+                            a.insert(0,Key_word)
+                            #print(a)
+                            #new=Key_word[i] , a
+                            m.append(a)
+    if len(m)>0:
+        print(True)
+        data = pd.DataFrame(m)
+        data.to_excel("Items of" + Key_word + ".xlsx")
 
 
+trin3()
